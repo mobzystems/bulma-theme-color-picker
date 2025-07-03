8a57f8f04528149979cab39004be3ff3f4e14090
Markus The <markus@mobzystems.nl>
2025-07-03 14:21:28 +0200

    Grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 
-type BulmaType = 'primary' | 'link' | 'info' | 'success' | 'warning' | 'danger';
+type BulmaType = 'primary' | 'link' | 'info' | 'success' | 'warning' | 'danger' | 'light' | 'dark' | 'black' | 'white';
 // const bulmaTypes: string[] = ['primary', 'link', 'info', 'success', 'warning', 'danger'];
 
 function convertRgbToHsl(r: number, g: number, b: number): { h: number, s: number, l: number } {
@@ -54,19 +54,22 @@ function addStyle(style: any, type: BulmaType, color: string): any {
   style[`--bulma-${type}-l`] = hsl.l.toString() + '%';
 }
 
-function createInitialStyleMap(): Map<BulmaType, string> {
-  return new Map<BulmaType, string>([
+const defaultStyleMap = new Map<BulmaType, string>([
     ['primary', '#00d1b2'],
     ['link', '#3273dc'],
     ['info', '#209cee'],
     ['success', '#23d160'],
     ['warning', '#ffdd57'],
-    ['danger', '#ff3860']
-  ]);
-}
+    ['danger', '#ff3860'],
+    ['light', '#f5f5f5'],
+    ['dark', '#363636'],
+    ['black', '#000000'],
+    ['white', '#ffffff']
+]);
+
 export default function App() {
   const [mainTheme, setMainTheme] = useState<'light' | 'dark'>('dark');
-  const [styleMap, setStyleMap] = useState<Map<BulmaType, string>>(createInitialStyleMap());
+  const [styleMap, setStyleMap] = useState<Map<BulmaType, string>>(defaultStyleMap);
 
   function updateColor(type: BulmaType, color: string) {
     let newMap = new Map(styleMap);
@@ -81,67 +84,48 @@ export default function App() {
       <div className="block container">
         <h1 className="title">
           Hello there
-          <button className="button is-pulled-right" onClick={() => setMainTheme('dark')}>Dark</button>
-          <button className="button is-pulled-right" onClick={() => setMainTheme('light')}>Light</button>
+          <button className="button is-pulled-right is-dark" onClick={() => setMainTheme('dark')}>Dark</button>
+          <button className="button is-pulled-right is-light" onClick={() => setMainTheme('light')}>Light</button>
         </h1>
         <p className="subtitle">This is a simple Bulma theme color picker.</p>
       </div>
-      <div className="block container">
+      <div className="block container typegrid">
+        {Array.from(styleMap).map(([type, _color]) => <div key={type}>
+          is-{type}
+        </div>)}
         {Array.from(styleMap).map(([type, color]) =>
           <ColorPicker key={type} type={type} color={color} onchange={newColor => updateColor(type, newColor)} />
         )}
+        <Sample styles={styleMap} />
+        {Array.from(styleMap).map(([type, color]) => <div key={type}>
+          <button className="button is-light is-small" disabled={defaultStyleMap.get(type)! === color} onClick={() => updateColor(type, defaultStyleMap.get(type)!)}>Reset</button>
+        </div>)}
       </div>
-      <Sample styles={styleMap} />
-      {/* <Sample styles={styleMap} theme="dark" /> */}
     </div>
   </>;
 }
 
 function ColorPicker(props: { type: BulmaType, color: string, onchange: (color: string) => void }) {
-
-  const [color, setColor] = useState(props.color);
-
-  const hsl = convertRgbToHsl(
-    parseInt(color.slice(1, 3), 16),
-    parseInt(color.slice(3, 5), 16),
-    parseInt(color.slice(5, 7), 16)
-  );
-
-  return (
-    <div className="field">
-      <label className="label"><b>{props.type}</b></label>
-      <div className="control">
-        <input type="color" value={color} onChange={(e) => { setColor(e.target.value); props.onchange(e.target.value); }} className="input" />
-      </div>
-      <p className="help">Current color: {color} ({hsl.h}, {hsl.s}, {hsl.l})</p>
-    </div>
-  );
+  return <input type="color" value={props.color} onChange={(e) => props.onchange(e.target.value)} className="input" />;
 }
 
-function Sample(props: { styles: Map<BulmaType, string> /*, theme: 'light' | 'dark' */ }) {
+function Sample(props: { styles: Map<BulmaType, string>}) {
   let style: React.CSSProperties = {};
   for (const [type, color] of props.styles) {
     addStyle(style, type, color);
   }
 
-  // theme-${props.theme}`} data-theme={props.theme}
-
   return (
-    <div className={`content`}>
-      <div className="block container" style={style}>
-        {Array.from(props.styles).map(([type, color]) => <div>
-          <button key={type} className={`button is-${type}`} title={`${type}: ${color}`}>{type}</button>
-          <button key={`${type}-inverted`} className={`button is-${type} is-inverted`} title={`${type}: ${color}`}>{type} inverted</button>
-          <button key={`${type}-outlined`} className={`button is-${type} is-outlined`} title={`${type}: ${color}`}>{type} outlined</button>
-        </div>)}
-        <div>
-          <button className="button">Normal</button>
-          <button className="button is-light">Light</button>
-          <button className="button is-dark">Dark</button>
-          <button className="button is-black">Black</button>
-          <button className="button is-white">White</button>
-        </div>
-      </div>
+    <div className="sample" style={style}>
+      {Array.from(props.styles).map(([type, color]) =>
+        <button key={type} className={`button is-${type}`} title={`${type}: ${color}`}>is-{type}</button>
+      )}
+      {Array.from(props.styles).map(([type, color]) => <div key={type}>
+        <button key={`${type}-inverted`} className={`button is-${type} is-inverted`} title={`${type}: ${color}`}>is-inverted</button>
+      </div>)}
+      {Array.from(props.styles).map(([type, color]) => <div key={type}>
+        <button key={`${type}-outlined`} className={`button is-${type} is-outlined`} title={`${type}: ${color}`}>is-outlined</button>
+      </div>)}
     </div>
   );
 }
