668bc9feba5383dcf171ad5eacf1d63df431d0e5
Markus The <markus@mobzystems.nl>
2025-07-03 16:58:43 +0200

    Refactor and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState, type CSSProperties } from 'react'
 
-type BulmaType = 'primary' | 'link' | 'info' | 'success' | 'warning' | 'danger' | 'light' | 'dark' | 'black' | 'white';
-// const bulmaTypes: string[] = ['primary', 'link', 'info', 'success', 'warning', 'danger'];
+type BulmaType = 'primary' | 'link' | 'info' | 'success' | 'warning' | 'danger' | 'light' | 'dark' | 'black' | 'white'; //  | 'text' | 'ghost';
 
-function convertRgbToHsl(r: number, g: number, b: number): { h: number, s: number, l: number } {
-  var rgb = [r / 255, g / 255, b / 255];
+function convertRgbToHsl(color: string): { h: string, s: string, l: string } {
+  var rgb = [parseInt(color.slice(1, 3), 16) / 255, parseInt(color.slice(3, 5), 16) / 255, parseInt(color.slice(5, 7), 16) / 255];
 
   var min = rgb[0], max = rgb[0], maxcolor = 0;
   for (let i = 0; i < rgb.length - 1; i++) {
@@ -13,11 +12,11 @@ function convertRgbToHsl(r: number, g: number, b: number): { h: number, s: numbe
   }
 
   let h = 0;
-  if (maxcolor == 0)
+  if (maxcolor === 0)
     h = (rgb[1] - rgb[2]) / (max - min);
-  else if (maxcolor == 1)
+  else if (maxcolor === 1)
     h = 2 + (rgb[2] - rgb[0]) / (max - min);
-  else if (maxcolor == 2)
+  else if (maxcolor === 2)
     h = 4 + (rgb[0] - rgb[1]) / (max - min);
 
   if (isNaN(h))
@@ -30,41 +29,29 @@ function convertRgbToHsl(r: number, g: number, b: number): { h: number, s: numbe
   let l = (min + max) / 2;
 
   let s;
-  if (min == max)
+  if (min === max)
     s = 0;
   else if (l < 0.5)
     s = (max - min) / (max + min);
   else
     s = (max - min) / (2 - max - min);
 
-  // s = s;
-  return { h: Math.round(h), s: Math.round(s * 1000) / 10, l: Math.round(l * 1000) / 10 };
-
-  // return `hsl(${Math.round(h * 360)}, ${Math.round(s * 100)}%, ${Math.round(l * 100)}%)`;
-}
-
-function addStyle(style: any, type: BulmaType, color: string): any {
-  const hsl = convertRgbToHsl(
-    parseInt(color.slice(1, 3), 16),
-    parseInt(color.slice(3, 5), 16),
-    parseInt(color.slice(5, 7), 16)
-  );
-  style[`--bulma-${type}-h`] = hsl.h.toString();
-  style[`--bulma-${type}-s`] = hsl.s.toString() + '%';
-  style[`--bulma-${type}-l`] = hsl.l.toString() + '%';
+  return { h: `${Math.round(h)}deg`, s: `${Math.round(s * 1000) / 10}%`, l: `${Math.round(l * 1000) / 10}%` };
 }
 
 const defaultStyleMap = new Map<BulmaType, string>([
-    ['primary', '#00d1b2'],
-    ['link', '#3273dc'],
-    ['info', '#209cee'],
-    ['success', '#23d160'],
-    ['warning', '#ffdd57'],
-    ['danger', '#ff3860'],
-    ['light', '#f5f5f5'],
-    ['dark', '#363636'],
-    ['black', '#000000'],
-    ['white', '#ffffff']
+  ['primary', '#00d1b2'],
+  ['link', '#3273dc'],
+  ['info', '#209cee'],
+  ['success', '#23d160'],
+  ['warning', '#ffdd57'],
+  ['danger', '#ff3860'],
+  ['light', '#f5f5f5'],
+  ['dark', '#363636'],
+  ['black', '#000000'],
+  ['white', '#ffffff'],
+  // ['text', '#4a4a4a'],
+  // ['ghost', '#f5f5f5']
 ]);
 
 export default function App() {
@@ -79,29 +66,63 @@ export default function App() {
 
   useEffect(() => document.documentElement.setAttribute('data-theme', mainTheme), [mainTheme]);
 
+  function createStyleSheet() {
+    return (`@import url(https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css);
+
+/* Bulma overrides */
+:root {${Array.from(styleMap).map(([type, color]) => {
+      const hsl = convertRgbToHsl(color);
+      return `
+  /* ${type}: ${color} (${color === defaultStyleMap.get(type)! ? 'default' : 'customized'}) */
+  --bulma-${type}-h: ${hsl.h};
+  --bulma-${type}-s: ${hsl.s};
+  --bulma-${type}-l: ${hsl.l};
+`;
+    }).join('').trimEnd()}
+}`);
+  }
+
+  let style: React.CSSProperties = {};
+  for (const [type, color] of styleMap) {
+    const hsl = convertRgbToHsl(color);
+    (style as any)[`--bulma-${type}-h`] = hsl.h;
+    (style as any)[`--bulma-${type}-s`] = hsl.s;
+    (style as any)[`--bulma-${type}-l`] = hsl.l;
+  }
+
   return <>
-    <div className="section is-smnall is-info" data-theme={mainTheme}>
-      <div className="block container">
+    <div className="section">
+      <div className="container">
         <h1 className="title">
-          Hello there
+          Bulma Theme Color Picker
           <button className="button is-pulled-right is-dark" onClick={() => setMainTheme('dark')}>Dark</button>
           <button className="button is-pulled-right is-light" onClick={() => setMainTheme('light')}>Light</button>
         </h1>
-        <p className="subtitle">This is a simple Bulma theme color picker.</p>
+        <p className="subtitle">A simple Bulma theme color picker by <a href="https://www.mobzystems.com" target="_blank">MOBZystems</a></p>
       </div>
-      <div className="block container typegrid">
+    </div>
+    <div className="section">
+      <div className="container typegrid">
         {Array.from(styleMap).map(([type, _color]) => <div key={type}>
-          is-{type}
+          <b>{type}</b>
         </div>)}
         {Array.from(styleMap).map(([type, color]) =>
           <ColorPicker key={type} type={type} color={color} onchange={newColor => updateColor(type, newColor)} />
         )}
-        <Sample styles={styleMap} />
+        <ButtonGrid styles={styleMap} style={style} />
         {Array.from(styleMap).map(([type, color]) => <div key={type}>
           <button className="button is-light is-small" disabled={defaultStyleMap.get(type)! === color} onClick={() => updateColor(type, defaultStyleMap.get(type)!)}>Reset</button>
         </div>)}
       </div>
     </div>
+    <div className="section">
+      <div className="container">
+        <h2 className="title is-4">Generated CSS</h2>
+        <p className="subtitle is-6">Copy the CSS below to your project's style sheet</p>
+        <textarea className="textarea is-family-monospaced" readOnly rows={10} value={createStyleSheet()}></textarea>
+      </div>
+    </div>
+    <Sample style={style} />
   </>;
 }
 
@@ -109,23 +130,88 @@ function ColorPicker(props: { type: BulmaType, color: string, onchange: (color: 
   return <input type="color" value={props.color} onChange={(e) => props.onchange(e.target.value)} className="input" />;
 }
 
-function Sample(props: { styles: Map<BulmaType, string>}) {
-  let style: React.CSSProperties = {};
-  for (const [type, color] of props.styles) {
-    addStyle(style, type, color);
+function ButtonGrid(props: { styles: Map<BulmaType, string>, style: CSSProperties }) {
+  function ButtonList(p: { formatValue: (type: BulmaType) => string, extraStyle?: string }) {
+    return Array.from(props.styles).map(([type, color]) =>
+      <button key={type} className={`button is-${type}${p.extraStyle ? ` ${p.extraStyle}` : ''}`} title={`${type}: ${color}`}>{p.formatValue(type)}</button>
+    );
   }
-
   return (
-    <div className="sample" style={style}>
-      {Array.from(props.styles).map(([type, color]) =>
-        <button key={type} className={`button is-${type}`} title={`${type}: ${color}`}>is-{type}</button>
-      )}
-      {Array.from(props.styles).map(([type, color]) => <div key={type}>
-        <button key={`${type}-inverted`} className={`button is-${type} is-inverted`} title={`${type}: ${color}`}>is-inverted</button>
-      </div>)}
-      {Array.from(props.styles).map(([type, color]) => <div key={type}>
-        <button key={`${type}-outlined`} className={`button is-${type} is-outlined`} title={`${type}: ${color}`}>is-outlined</button>
-      </div>)}
+    <div className="buttongrid" style={props.style}>
+      <ButtonList formatValue={(type) => `${type}`} />
+      <ButtonList formatValue={_type => 'inverted'} extraStyle="is-inverted" />
+      <ButtonList formatValue={_type => 'outlined'} extraStyle="is-outlined" />
+      <ButtonList formatValue={_type => 'light'} extraStyle="is-light" />
+      <ButtonList formatValue={_type => 'dark'} extraStyle="is-dark" />
+      {/* <ButtonList formatValue={_type => 'is-text'} extraStyle="is-text" /> */}
+      {/* <ButtonList formatValue={_type => 'is-ghost'} extraStyle="is-ghost" /> */}
+    </div>
+  );
+}
+
+function Sample(props: { style: CSSProperties }) {
+  return (
+    <div className="sample section" style={props.style}>
+      <div className="block container">
+        <p className="title">Title</p>
+        <p className="subtitle">Subtitle</p>
+        <div className="notification is-info">
+          <button className="delete"></button>
+          <p>Notification: This is a message</p>
+        </div>
+        <div className="notification is-success">
+          <button className="delete"></button>
+          <p>Notification: Operation completed successfully</p>
+        </div>
+        <div className="notification is-warning">
+          <button className="delete"></button>
+          <p>Notification: Operation completed with warnings</p>
+        </div>
+        <div className="notification is-danger">
+          <button className="delete"></button>
+          <p>Error notification</p>
+        </div>
+
+        <article className="message is-info">
+          <div className="message-body">
+            Information message
+          </div>
+        </article>
+        <article className="message is-success">
+          <div className="message-header">
+            <p>Success</p>
+            <button className="delete" aria-label="delete"></button>
+          </div>
+          <div className="message-body">
+            Succes message
+          </div>
+        </article>
+        <article className="message is-warning">
+          <div className="message-body">
+            Warning message
+          </div>
+        </article>
+        <article className="message is-danger">
+          <div className="message-body">
+            Error message
+          </div>
+        </article>
+      </div>
+
+      <div className="block container content">
+        <p>This is how your custom theme looks. <a href="/" onClick={e => e.preventDefault()}>This is a link</a></p>
+        <div className="buttons">
+          <button className="button is-primary">OK</button>
+          <button className="button is-link">More...</button>
+          <button className="button is-info">See also</button>
+          <button className="button is-success">Success</button>
+          <button className="button is-warning">Warning</button>
+          <button className="button is-danger">Danger</button>
+          <button className="button is-text">Text</button>
+          <button className="button is-ghost">Ghost</button>
+          <button className="button is-static">Static</button>
+        </div>
+      </div>
     </div>
   );
 }
